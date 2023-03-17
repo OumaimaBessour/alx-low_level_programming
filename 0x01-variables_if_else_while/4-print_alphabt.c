8086e730aa9c1a7f03d78db5366dddd18276e166
OumaimaBessour <hajarbessour96@gmail.com>
2023-03-17 15:11:53 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
+
 /**
- * main - Prints the numbers from 0-9.
-(*
- * Return: 0 on success
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Sucess)
  */
+
 int main(void)
 {
-	printf("0123456789\n");
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		if ((c == 'q' || c == 'e') != 1)
+		{
+			putchar(c);
+		}
+
+	}
+	putchar('\n');
 	return (0);
 }
